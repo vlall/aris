6ec709cabb934242600fc27929ae879a2f1946e9
Vishal <vlall@users.noreply.github.com>
2015-06-26 18:18:25 -0400

    Update ip_utils.py

--- ip_utils.py
+++ ip_utils.py
@@ -7,24 +7,24 @@ class Ip_Check:
 
 	def __init__(self):
 			
-			public_IP = commands.getstatusoutput('curl -s checkip.dyndns.org | sed -e \'s/.*Current IP Address: //\' -e \'s/<.*$//\'') 
-			local_IP = commands.getstatusoutput('ifconfig | grep -Eo \'inet (addr:)?([0-9]*\.){3}[0-9]*\' | grep -Eo \'([0-9]*\.){3}[0-9]*\' | grep -v \'127.0.0.1\'')
-			self.public_IP = public_IP[1]
-			self.local_IP=str(local_IP[1])
+		public_IP = commands.getstatusoutput('curl -s checkip.dyndns.org | sed -e \'s/.*Current IP Address: //\' -e \'s/<.*$//\'') 
+		local_IP = commands.getstatusoutput('ifconfig | grep -Eo \'inet (addr:)?([0-9]*\.){3}[0-9]*\' | grep -Eo \'([0-9]*\.){3}[0-9]*\' | grep -v \'127.0.0.1\'')
+		self.public_IP = public_IP[1]
+		self.local_IP=str(local_IP[1])
 
 	def EOS_login(self, hostname='arista', hostpass='arista'):
-			try:
-				'''
-				>admin
-				>en
-				>conf t
-				>show vlan
-				os.system('ssh' + hostname)
-				password = (hostpass)
-				'''
-				print ('Connection successful!')
-			except Exception:
-				print('Connection failed')
+		try:
+			'''
+			>admin
+			>en
+			>conf t
+			>show vlan
+			os.system('ssh' + hostname)
+			password = (hostpass)
+			'''
+			print ('Connection successful!')
+		except Exception:
+			print('Connection failed')
 	
 	def connection(self):
 		pass
